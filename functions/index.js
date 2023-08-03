8934509051ad671e95100ad743a835bbe836833a
pho0m <pho0m2000s@gmail.com>
2023-08-03 07:47:52 +0700

    feat(line-bot): add new api for line api
    \

--- functions/index.js
+++ functions/index.js
@@ -49,7 +49,103 @@ const reply = (bodyResponse) => {
           type: `text`,
           text: bodyResponse.events[0].message.text,
         },
+        {
+          type: "text", // ①
+          text: "Select your favorite food category or send me your location!",
+          quickReply: {
+            // ②
+            items: [
+              {
+                type: "action", // ③
+                imageUrl: "https://example.com/sushi.png",
+                action: {
+                  type: "message",
+                  label: "Sushi",
+                  text: "Sushi",
+                },
+              },
+              {
+                type: "action",
+                imageUrl: "https://example.com/tempura.png",
+                action: {
+                  type: "message",
+                  label: "Tempura",
+                  text: "Tempura",
+                },
+              },
+              {
+                type: "action", // ④
+                action: {
+                  type: "location",
+                  label: "Send location",
+                },
+              },
+            ],
+          },
+        },
       ],
     }),
   });
 };
+
+exports.LineBotPush = onRequest((req, res) => {
+  return request({
+    method: `GET`,
+    headers: {
+      "X-RapidAPI-Key": "e11548470cmsh47de7c0b08636d3p11b4cajsn0065b2b4b88e",
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+    uri: `https://weatherapi-com.p.rapidapi.com/current.json?q=13.819314097361195,100.51429852634443`,
+  })
+    .then((response) => {
+      const data = JSON.parse(response);
+
+      //TODO: close here before deploy firebase functions
+      // console.log(
+      //   "========================== location =================================="
+      // );
+      // console.log(data.location);
+      // console.log(
+      //   "============================ current ================================"
+      // );
+      // console.log(data.current);
+      // console.log(
+      //   "============================================================"
+      // );
+
+      const message = `Report Now!
+      \n- location: ${data.location.name}\n- region: ${data.location.region}\n- last_updated: ${data.current.last_updated}\n- temp_c: ${data.current.temp_c} (° C)`;
+
+      // res.json(message);
+      return push(res, message);
+    })
+    .catch((error) => {
+      return res.status(500).send(error);
+    });
+});
+
+const push = async (res, msg) => {
+  try {
+    await request({
+      method: `POST`,
+      uri: `${LINE_MESSAGING_API}/push`,
+      headers: LINE_HEADER,
+      body: JSON.stringify({
+        to: `U91c3b119450a3b494055bafb98c1a484`,
+        messages: [
+          {
+            type: `text`,
+            text: msg,
+          },
+        ],
+      }),
+    });
+    return res.status(200);
+  } catch (error) {
+    return await Promise.reject(error);
+  }
+};
+
+exports.HelloWorld = onRequest((req, res) => {
+  res.send("Hello World!");
+});
